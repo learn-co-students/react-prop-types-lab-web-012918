7b8e6639eac1de73966e23b6d23a039c91d26336
allinthemind66 <r.nilon92@gmail.com>
2018-04-02 10:05:46 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,46 @@
 // Code Product Component Here
+import React from 'react'
+import PropTypes from 'prop-types';
+class Product extends React.Component {
+  render(){
+    const {name, producer, hasWatermark, color, weight} = this.props
+    return (
+      <div>
+        <h1>{name}</h1>
+        <p>{producer}</p>
+        <p>{hasWatermark}</p>
+        <p>{color}</p>
+        <p>{weight}</p>
+      </div>
+    )
+  }
+}
+
+
+
+Product.defaultProps = {
+  hasWatermark: false
+}
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: (props, propName)=> {
+    let value = props[propName]
+    if (value === undefined){
+      return new Error ("Needs Number")
+    }
+    if (isNaN(value)) {
+      return new Error ("Needs to be a number")
+    }
+    let correctRange = (value > 80 && value < 300)
+    if(!correctRange){
+      return new Error ("not correct range")
+    }
+
+  }
+}
+
+
+export default Product
