8fb463586d846cda27e6e9cc95de5a856b0906ae
izzee <ichabon@me.com>
2018-04-03 15:07:58 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,6 +4,15 @@ import PropTypes from 'prop-types';
 
 class Product extends React.Component {
   render(){
+    return (
+      <div>
+        <p>{this.props.name}</p>
+        <p>{this.props.producer}</p>
+        <p>{this.props.hasWatermark ? "yes" : "no"}</p>
+        <p>{this.props.color}</p>
+        <p>{this.props.weight}</p>
+      </div>
+    )
   }
 }
 
